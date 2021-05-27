e12a7fc7a3f89ad586ffc93e5230f36dba45e78d
Nimesh Alawatta <hirudika99@gmail.com>
2021-05-27 14:54:59 +0530

    Updates

---
+++ precache-manifest.85e0995b3d4b4b80582b447bedd56fad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ea8e19fbfea32d1d412",
+    "url": "/react-appointment-handler/static/css/main.05fcd224.chunk.css"
+  },
+  {
+    "revision": "9ea8e19fbfea32d1d412",
+    "url": "/react-appointment-handler/static/js/main.5e0c2c6c.chunk.js"
+  },
+  {
+    "revision": "d8bd6c83f6d95e616329",
+    "url": "/react-appointment-handler/static/js/runtime~main.128250ab.js"
+  },
+  {
+    "revision": "9bb9138f9a20fa6cf8eb",
+    "url": "/react-appointment-handler/static/css/2.4433fbb2.chunk.css"
+  },
+  {
+    "revision": "9bb9138f9a20fa6cf8eb",
+    "url": "/react-appointment-handler/static/js/2.aef344c6.chunk.js"
+  },
+  {
+    "revision": "622b3b9a302fd6b4567d903a2298d72d",
+    "url": "/react-appointment-handler/index.html"
+  }
+];
